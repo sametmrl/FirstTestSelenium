f0a8213ce3b276eddc5c1e40281f5d66ff00fd14
sametmrl <44403365+sametmrl@users.noreply.github.com>
2018-12-03 09:25:35 +0200

    Update KayitIslemi.java

--- src/test/java/testler/DemoBorlandKayıtİslemi/KayitIslemi.java
+++ src/test/java/testler/DemoBorlandKayıtİslemi/KayitIslemi.java
@@ -20,6 +20,21 @@ pipeline {
     }
 }
 
+pipeline {
+    agent any
+    stages {
+        stage('Test') {
+            steps {
+                sh './gradlew check'
+            }
+        }
+    }
+    post {
+        always {
+            junit 'build/reports/**/*.xml'
+        }
+    }
+}
 @Feature("Antet İşlemleri")
 public class KayitIslemi {
 
